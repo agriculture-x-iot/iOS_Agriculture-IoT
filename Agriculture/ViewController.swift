ef5fecf0941dfe48eacb78358a303cee69f06e02
fujimori0220 <G016C1102@outlook.jp>
2019-06-28 11:30:19 +0900

    Initial Commit

---
+++ Agriculture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Agriculture
+//
+//  Created by 藤森大輝 on 2019/06/28.
+//  Copyright © 2019 藤森大輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
